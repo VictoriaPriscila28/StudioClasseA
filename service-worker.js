a6f11304a194fb8e8c857cb4cb51975fdbdd00c8
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-03-20 22:36:09 -0300

    trying to transform in a pwa

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,41 @@
-var cacheName = 'JustDance+-v1.0';
+var cacheName = 'JustDance-v1.0'; // Corrigido o nome do cache
 
 self.addEventListener('install', event => {
-
   self.skipWaiting();
 
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-
         './index.html',
-
+        './about.html', // Nome do arquivo corrigido
+        './contact.html',
+        './classes.html',
+        './event.html',
+        './team.html',
+        './testimonial.html',
+        './training.html',
+        './gallery.html',
+        './blog.html',
         './css/bootstrap.min.css',
-
-        
-
-        './img/about-2.png',
-        
       ]))
   );
 });
 
-self.addEventListener('message', function (event) {
+self.addEventListener('message', function(event) {
   if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
 
 self.addEventListener('fetch', function (event) {
-  //Atualizacao internet
-  event.respondWith(async function () {
-    try {
-      return await fetch(event.request);
-    } catch (err) {
-      return caches.match(event.request);
-    }
-  }());
-
-  //Atualizacao cache
-  /*event.respondWith(
+  // Estratégia de cache-first: tenta buscar no cache primeiro
+  event.respondWith(
     caches.match(event.request)
       .then(function (response) {
         if (response) {
-          return response;
+          return response; // Retorna do cache se encontrado
         }
-        return fetch(event.request);
+        return fetch(event.request); // Busca na internet se não estiver no cache
       })
-  );*/
-
+  );
 });
