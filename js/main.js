104fe26f9257a2f707635342d57070fd41680cfd
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-04-05 12:10:06 -0300

    add push notification

--- js/main.js
+++ js/main.js
@@ -243,7 +243,41 @@
         return false;
     });
 
-    // geolocation
+    // permission
+    var btnPermission = document.getElementById("btn_permission");
+    if (Notification.permission !== "default") {
+        btnPermission.style.display = "none";
+    } else {
+        btnPermission.style.display = "inline-block";
+    }
+
+    btnPermission.onclick = () => {
+        Notification.requestPermission();
+    };
+    // notificações
+    function spawnNotification(options) {
+        var n = new Notification(options.title, options.opt);
+        if (options.link !== "") {
+            n.addEventListener("click", function() {
+                n.close();
+                window.focus();
+                window.location.href = options.link;
+            });
+        }
+    }
+    // exemplo
+    // Exemplo de uso
+    spawnNotification({
+        title: "Bem vindo ao site!",
+        opt: {
+            body: "Espero que você aproveite para nos visitar em breve!",
+            icon: "img/android/icon", // Ícone opcional
+        },
+        link: "/detalhes-da-venda" // Link opcional
+    });
+
+    
+
     
     
    
